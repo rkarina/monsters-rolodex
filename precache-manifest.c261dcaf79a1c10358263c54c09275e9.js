ab424feee035a9d9acc49cd7c9f3fa89b8e0de3c
rkarina <rahmadhysk@gmail.com>
2019-08-08 06:49:42 +1000

    Updates

---
+++ precache-manifest.c261dcaf79a1c10358263c54c09275e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1153fcbffd1d4910336db82883be3046",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eff139aa21ef7fae86ff",
+    "url": "/monsters-rolodex/static/css/main.6bced024.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "eff139aa21ef7fae86ff",
+    "url": "/monsters-rolodex/static/js/main.6e40ac9d.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
